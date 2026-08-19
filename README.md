bdce597012e073c0f2bb0774539e1f3c068e8784
bettywiens <bwiens@hansalog.de>
2026-08-19 12:15:50 +0200

    README Ergänzung; Startprojekt JavaScript

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # HelloWorld
+
+Dieses repository enthält ein Hello-World Programm als auch je ein kleines Startprojekt in C#, Java/Processing, JavaScript und TypeScript.
---
+++ Startprojekte/JavaScript Startprojekt/myScript.js
@@ -0,0 +1,7 @@
+function dog() {
+    document.getElementById('image').src='images/dog.jpg';
+}
+
+function cat() {
+    document.getElementById('image').src='images/cat.jpg';
+}
